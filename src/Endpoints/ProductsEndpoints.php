2662e64751b3e23e693d47097325218f4ff09eed
mhrdlicka <martin.hrdlicka@mall.cz>
2016-07-21 11:13:27 +0200

    MAR-1138 MAR-1164 added method for get list of products

---
+++ src/Endpoints/ProductsEndpoints.php
@@ -0,0 +1,38 @@
+<?php
+namespace MPAPI\Endpoints;
+
+use GuzzleHttp\Psr7\Response;
+use MPAPI\Services\Client;
+
+/**
+ * Products endpoints
+ *
+ * @author Martin Hrdlicka <martin.hrdlicka@mall.cz>
+ */
+class ProductsEndpoints
+{
+	/**
+	 * @var string
+	 */
+	const ENDPOINT_PRODUCTS = 'products';
+
+	/**
+	 * @var Client
+	 */
+	private $client;
+
+	public function __construct(Client $client)
+	{
+		$this->client = $client;
+	}
+
+	/**
+	 * Get list of all products.
+	 *
+	 * @return Response
+	 */
+	public function getProducts()
+	{
+		return $this->client->sendRequest(self::ENDPOINT_PRODUCTS, 'GET');
+	}
+}
