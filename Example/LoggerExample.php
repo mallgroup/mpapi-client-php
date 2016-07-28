be346e19bce09d2723dbaea7b438c53cbb99687c
Jan Bláha <jan.blaha@mall.cz>
2016-07-28 14:20:29 +0200

    MAR-1137 - small change of example

--- Example/LoggerExample.php
+++ Example/LoggerExample.php
@@ -8,9 +8,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // initialize API client
 $mpApiClient = new Client('mp_vivantis_sk_dGVzc3R8MzAw');
 
-/**
- * Create instance of monolog logger
- */
+// Create instance of monolog logger
 $logger = new Logger('loggerName');
 $logger->pushHandler(new StreamHandler('/var/log/elog.log', Logger::INFO));
 
