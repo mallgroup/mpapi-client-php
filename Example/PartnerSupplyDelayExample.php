64d661d5b7ae3e8ccf871c4ebd314badfb29ad29
Martin Drlík <martin.drlik@mall.cz>
2017-01-12 15:14:29 +0100

    MAR-1935  MAR-2234 MPAPIC: update examples

---
+++ Example/PartnerSupplyDelayExample.php
@@ -0,0 +1,75 @@
+<?php
+use MPAPI\Services\Client;
+use MPAPI\Services\Partner;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$mpapiClient = new Client('api_marketplace_test_client_id');
+$logger = new Logger('loggerName');
+$logger->pushHandler(new StreamHandler('./elog.log', Logger::INFO));
+
+// set logger into MP API client
+$mpapiClient->setLogger($logger);
+
+// Initialize partner service
+$partner = new Partner($mpapiClient);
+
+/**
+ * #################################
+ *  SETUP NEW PARTNER SUPPLY DELAY
+ * #################################
+ */
+// create object with actual date/time
+$validFrom = new DateTime();
+$validTo = new DateTime();
+// modify date - add 10 days
+$validTo->modify('+10 day');
+
+/**
+ * You can send only end of validity
+ */
+$delayCreated = $partner->postSupplyDelay($validTo);
+print('Setup supply delay: ');
+var_dump($delayCreated);
+print(PHP_EOL);
+
+/**
+ * or you can send both valid from and valid to date
+ */
+$delayCreated2 = $partner->postSupplyDelay($validTo, $validFrom);
+print('Setup supply delay with both dates: ');
+var_dump($delayCreated2);
+print(PHP_EOL);
+
+/**
+ * #######################################
+ *  UPDATE EXISTING PARTNER SUPPLY DELAY
+ * #######################################
+ */
+$updatedValidTo = $validTo->modify('+5 day');
+$delayUpdated = $partner->putSupplyDelay($updatedValidTo);
+print('Update supply delay: ');
+var_dump($delayUpdated);
+print(PHP_EOL);
+
+/**
+ * #############################
+ *   GET PARTNER SUPPLY DELAY
+ * #############################
+ */
+$delayDetail = $partner->getSupplyDelay();
+print('Get supply delay: ');
+var_dump($delayDetail);
+print(PHP_EOL);
+
+/**
+ * ###############################
+ *   DELETE PARTNER SUPPLY DELAY
+ * ###############################
+ */
+$deleteDelay = $partner->deleteSupplyDelay();
+print('Delete supply delay: ');
+var_dump($deleteDelay);
+print(PHP_EOL);
