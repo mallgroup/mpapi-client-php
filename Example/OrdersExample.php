3228631bf83c95ee097927a1cfeaf53e23dd7db6
Martin Hrdlicka <martin.hrdlicka@mall.cz>
2017-03-14 09:56:10 +0100

    MAR-2315 MAR-2580 updated orders example

--- Example/OrdersExample.php
+++ Example/OrdersExample.php
@@ -24,6 +24,41 @@ foreach ($openOrders as $orderId) {
 	print('Open order: ' . $orderId . PHP_EOL);
 }
 
+// get all shipping orders
+$shippingOrders = $orders->get()->shipping();
+// print of shipping orders
+foreach ($shippingOrders as $orderId) {
+	print('Shipping order: ' . $orderId . PHP_EOL);
+}
+
+// get all shipped orders
+$shippedOrders = $orders->get()->shipped();
+// print of shipped orders
+foreach ($shippedOrders as $orderId) {
+	print('Shipped order: ' . $orderId . PHP_EOL);
+}
+
+// get all delivered orders
+$deliveredOrders = $orders->get()->delivered();
+// print of delivered orders
+foreach ($deliveredOrders as $orderId) {
+	print('Delivered order: ' . $orderId . PHP_EOL);
+}
+
+// get all returned orders
+$returnedOrders = $orders->get()->returned();
+// print of returned orders
+foreach ($returnedOrders as $orderId) {
+	print('Returned order: ' . $orderId . PHP_EOL);
+}
+
+// get all cancelled orders
+$cancelledOrders = $orders->get()->cancelled();
+// print of cancelled orders
+foreach ($cancelledOrders as $orderId) {
+	print('Cancelled order: ' . $orderId . PHP_EOL);
+}
+
 // get all unconfirmed orders
 $unconfirmedOrders = $orders->get()->unconfirmed();
 // print list of unconfirmed orders
